2064a55726866076bc0c78a63fb22d3643e25ee1
Quique Posada <quiqueposadalozano@Quique-Posadas-MacBook-Pro.local>
2018-10-26 09:56:40 -0500

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   #before_action :logged_in_user, only: [:edit, :update]
   #before_action :logged_in_user, only: [:edit, :update]
   #before_action :logged_in_user, only: [:index, :edit, :update]
+
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
@@ -14,6 +15,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
     #debugger
   end
 
@@ -84,6 +86,8 @@ class UsersController < ApplicationController
       redirect_to(root_url) unless @user == current_user
     end
 
+
+#Hay uno repetido que se va a quitar si da problema en las pruebas
   # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
@@ -94,5 +98,8 @@ class UsersController < ApplicationController
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
+
+
+
   
 end
